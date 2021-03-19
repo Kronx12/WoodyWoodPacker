6466f27d79db4d7606bae2db8e5a2e8cf20ece1f
Kronx12 <kronx12pro@gmail.com>
2021-03-19 01:21:00 +0100

    Update structure

--- sources/include/global.h
+++ sources/include/global.h
@@ -6,7 +6,7 @@
 /*   By: gbaud <gbaud@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 19:32:18 by gbaud             #+#    #+#             */
-/*   Updated: 2021/03/17 20:15:10 by gbaud            ###   ########lyon.fr   */
+/*   Updated: 2021/03/17 21:05:45 by gbaud            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,29 @@
 #define FILETYPE_ERROR "\e[1;31mWoody_Woodpacker: file is a directory\n\e[0m"
 
 #define put_err(err) ({ printf("\e[1;31m%s (%s)\n\e[0m", err, strerror(errno)); })
+
+/**
+ * @brief Check if file exist
+ * 
+ * @param file
+ * @return int 
+ */
+int file_exist(char *file);
+
+/**
+ * @brief Check if file is directory
+ * 
+ * @param file
+ * @return int 
+ */
+int file_is_file(char *file);
+
+/**
+ * @brief Check if file is elf64
+ * 
+ * @param file 
+ * @return int 
+ */
+int file_is_elf64(char *file);
 
 #endif
